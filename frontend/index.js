f3d53b20bb881bc1a4f86597ed50564b475bc192
Deekshita2327 <24wh1a05r7@bvrithyderabad.edu.in>
2026-07-04 10:01:30 +0530

    initial commit

--- frontend/index.js
+++ frontend/index.js
@@ -25,3 +25,23 @@ function toggle(){
 console.log("Hello from JS");
 
 alert("Hello from JS");
+function getRandomUser() {
+    fetch("https://randomuser.me/api")
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function(parsedData) {
+
+            let gender = parsedData.results[0].gender;
+
+            let first = parsedData.results[0].name.first;
+            let last = parsedData.results[0].name.last;
+            let name = first + " " + last;
+
+            let imageUrl = parsedData.results[0].picture.large;
+
+            document.getElementById("card-gender").innerText = gender;
+            document.getElementById("card-name").innerText = name;
+            document.getElementById("img").src = imageUrl;
+        });
+}
