cf9753fef543476efd280d3d7ebfdc0bc544c9db
Deekshita2327 <24wh1a05r7@bvrithyderabad.edu.in>
2026-07-04 09:44:15 +0530

    initial commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,27 @@
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "./image/jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "./image/john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+}
+
 console.log("Hello from JS");
+
 alert("Hello from JS");
